af4327233fa0205bcf823d24849bea9b0d604a57
Jessica Greene <jessica0greene@gmail.com>
2017-10-03 12:39:26 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,8 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 let index = 0;
 function init() {
   const body = document.getElementsByTagName('body');
-  console.log(body)
-  body.addEventListner('keydown', function(e){
+  body.addEventListener('keydown', function(e) {
     const key = parseInt(e.detail);
     console.log(key)
     if (key === code[index]) {
