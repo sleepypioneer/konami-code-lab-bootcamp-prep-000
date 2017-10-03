854c09516474878fb9f5d2d11c1e304399eee050
Jessica Greene <jessica0greene@gmail.com>
2017-10-03 12:31:05 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,10 +1,10 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 let index = 0;
 function init() {
-  const body = document.getElementByTagName('body');
-  body.addEventListner('keydown', function(event){
-    const key = parseInt(event.detail || event.location);
-
+  const body = document.getElementsByTagName('body');
+  body.addEventListner('keydown', function(e){
+    const key = parseInt(e.detail || e.which);
+    console.log(key)
     if (key === code[index]) {
       index++;
 
@@ -18,3 +18,5 @@ function init() {
 
   })
 }
+
+init()
